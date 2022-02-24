becaf0c6c1d6b1507a0b051ed7fac30a18a470dc
Paolo4690 <paolo.calafiore.90@gmail.com>
2022-02-24 15:58:14 +0100

    add img dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -3,10 +3,43 @@ const valUser = document.querySelector('.you');
 const risultato = document.querySelector('.result');
 const btnRefresh = document.querySelector('.refresh');
 
+let imgDadoPc = document.getElementById("imagePc");
+let imgDadoYou = document.getElementById("imageYou");
+
 let valoreDadiComputer = Math.floor(Math.random() * 6) + 1;
 let valoreDadiTu = Math.floor(Math.random() * 6) + 1;
-valComputer.innerHTML = 'Il Computer ha tirato: ' + valoreDadiComputer;
-valUser.innerHTML = 'Tu hai tirato: ' + valoreDadiTu;
+
+valComputer.innerHTML = 'Il Computer ha tirato:';
+valUser.innerHTML = 'Tu hai tirato:';
+
+switch (valoreDadiComputer) {
+    case 1:imgDadoPc.src = "img/1.png";
+    break;
+    case 2:imgDadoPc.src = "img/2.png";
+    break;
+    case 3:imgDadoPc.src = "img/3.png";
+    break;
+    case 4:imgDadoPc.src = "img/4.png";
+    break;
+    case 5:imgDadoPc.src = "img/5.png";
+    break;
+    case 6:imgDadoPc.src = "img/6.png";
+    break;
+}
+switch (valoreDadiTu) {
+    case 1:imgDadoYou.src = "img/1.png";
+    break;
+    case 2:imgDadoYou.src = "img/2.png";
+    break;
+    case 3:imgDadoYou.src = "img/3.png";
+    break;
+    case 4:imgDadoYou.src = "img/4.png";
+    break;
+    case 5:imgDadoYou.src = "img/5.png";
+    break;
+    case 6:imgDadoYou.src = "img/6.png";
+    break;
+}
 
 if (valoreDadiComputer > valoreDadiTu) {
     risultato.innerHTML = 'Il Computer ha vinto';
@@ -19,3 +52,13 @@ if (valoreDadiComputer > valoreDadiTu) {
 btnRefresh.addEventListener('click', function() {
     location.reload();
 })
+
+
+
+
+
+
+// switch ()
+//   imgDadoPc.src = "../img/1.jpg";
+//   document.body.appendChild(img);
+
